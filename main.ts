2407cdb314f371904892f811547006ce8f2b929c
Abdelrahman Mahmoud <47092261+Eng-Abdelrahman@users.noreply.github.com>
2020-11-27 22:12:04 +0200

    Sync Updates

--- main.ts
+++ main.ts
@@ -1,46 +1,46 @@
 
 
-class Point{
-    
-    constructor(public x:number , public y:number ){
+class Point {
+
+    constructor(public x: number, public y: number) {
 
     }
 }
 
-abstract class Shape{
-    P:Point;
-    Color:string;
-    constructor(p:Point , color:string){
-        this.P = p ;
-        this.Color = color ;        
+abstract class Shape {
+    P: Point;
+    Color: string;
+    constructor(p: Point, color: string) {
+        this.P = p;
+        this.Color = color;
     }
-    abstract drow():void;//must overriden
+    abstract drow(): void;//must overriden
 }
 
 
 //let ss = new Shape(new Point(2,3) , 'Red');
-class Circule extends Shape{
- Radious:number ;
- constructor(x:number , y:number, color:string , r:number){
-    super(new Point(x,y) , color);
-    this.Radious =r;      
- }
- drow():void{
-   
-    let circle =  document.getElementById("circle") as any;
+class Circule extends Shape {
+    Radious: number;
+    constructor(x: number, y: number, color: string, r: number) {
+        super(new Point(x, y), color);
+        this.Radious = r;
+    }
+    drow(): void {
 
-    circle.setAttribute("cx" , this.P.x.toString());
+        let circle = document.getElementById("circle") as any;
 
-    circle.setAttribute("cy" , this.P.y.toString());
+        circle.setAttribute("cx", this.P.x.toString());
 
-    circle.setAttribute("fill" , this.Color);
+        circle.setAttribute("cy", this.P.y.toString());
 
-    circle.setAttribute("r" , this.Radious.toString());
+        circle.setAttribute("fill", this.Color);
+
+        circle.setAttribute("r", this.Radious.toString());
 
 
- }
+    }
 }
 
-let c = new Circule(1000,500,'gray',250);
+let c = new Circule(1000, 500, 'gray', 250);
 
 c.drow();
